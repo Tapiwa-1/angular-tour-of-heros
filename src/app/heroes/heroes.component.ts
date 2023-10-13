516122df59fe9b2d42f850d03690163f5df22ec1
tapiwa-1 <tmotsi10@gmail.com>
2023-10-13 11:25:13 +0200

    adding http requests

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,26 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 // import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { Observable, of } from 'rxjs'; //Allow data to be fetched asynchrously
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
   selectedHero?: Hero;
   
+  
   constructor(private heroService: HeroService) {}
+  
   ngOnInit(): void {
     this.getHeroes();
   }
-  getHeroes(): void {
-    //get all heros
-    this.heroes = this.heroService.getHeroes();
+  // getHeroes(): void {
+  //   //get all heros from heroes service
+  //   this.heroes = this.heroService.getHeroes(); //assumes that this fetches data synchronously
+
+  // }
+
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
+  
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
